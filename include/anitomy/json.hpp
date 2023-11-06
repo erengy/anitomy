454de2c5a916d4ec76efb2bf6a154e924e36e6d1
Eren Okka <erenokka@gmail.com>
2023-11-07 01:29:48 +0300

    Improve JSON parser

--- include/anitomy/json.hpp
+++ include/anitomy/json.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <expected>
 #include <format>
 #include <functional>
 #include <map>
@@ -38,45 +39,70 @@ public:
 
   explicit Value() = default;
 
-  explicit Value(value_t&& value) : value_{std::move(value)} {
+  explicit Value(value_t&& value) : value_{value} {
   }
 
-  explicit Value(std::string_view input) : input_{input}, view_{input_}, value_{parse_value()} {
-  }
-
-  inline object_t& as_object() noexcept {
+  [[nodiscard]] inline object_t& as_object() noexcept {
     static object_t empty_object;
     return holds(Kind::Object) ? std::get<object_t>(value_) : empty_object;
   }
 
-  inline array_t& as_array() noexcept {
+  [[nodiscard]] inline array_t& as_array() noexcept {
     static array_t empty_array;
     return holds(Kind::Array) ? std::get<array_t>(value_) : empty_array;
   }
 
-  inline string_t as_string() const noexcept {
+  [[nodiscard]] inline string_t as_string() const noexcept {
     return holds(Kind::String) ? std::get<string_t>(value_) : string_t{};
   }
 
-  inline int as_number() const noexcept {
+  [[nodiscard]] inline int as_number() const noexcept {
     return holds(Kind::Number) ? std::get<int>(value_) : 0;
   }
 
-  inline bool as_bool() const noexcept {
+  [[nodiscard]] inline bool as_bool() const noexcept {
     return holds(Kind::Boolean) ? std::get<bool>(value_) : false;
   }
 
-  inline bool holds(const Kind kind) const noexcept {
+  [[nodiscard]] inline bool holds(const Kind kind) const noexcept {
     return value_.index() == kind;
   }
 
-  inline const value_t& value() const noexcept {
-    return value_;
+  [[nodiscard]] inline Kind kind() const noexcept {
+    return static_cast<Kind>(value_.index());
   }
 
 private:
-  inline value_t parse_value() noexcept {
-    const auto parse = [this]() -> value_t {
+  value_t value_;
+};
+
+class Parser final {
+public:
+  using string_t = Value::string_t;
+  using object_t = Value::object_t;
+  using array_t = Value::array_t;
+  using value_t = Value::value_t;
+
+  enum class Error {};
+
+  template <typename T>
+  using expected_t = std::expected<T, Error>;
+
+  explicit Parser(std::string_view input) : input_{input}, view_{input_} {
+  }
+
+  [[nodiscard]] inline Value parse() noexcept {
+    auto value = parse_value();
+    return Value{value ? std::move(*value) : object_t{}};
+  }
+
+private:
+  [[nodiscard]] static inline std::unexpected<Error> error() noexcept {
+    return std::unexpected{Error{}};
+  }
+
+  [[nodiscard]] inline expected_t<value_t> parse_value() noexcept {
+    const auto parse = [this]() -> expected_t<value_t> {
       if (view_.empty()) {
         return object_t{};
       }
@@ -98,71 +124,77 @@ private:
       if (peek() == 'n') {
         return parse_null();
       }
-      return nullptr;
+      return error();
     };
 
     skip_whitespace();
-    value_t value = parse();
+    auto value = parse();
     skip_whitespace();
     return value;
   }
 
-  inline object_t parse_object() noexcept {
+  [[nodiscard]] inline expected_t<object_t> parse_object() noexcept {
     object_t object;
 
-    skip('{');
+    if (!expect('{')) return error();
     skip_whitespace();
-    while (peek() != '}') {
+    while (!view_.empty() && peek() != '}') {
       skip_whitespace();
-      string_t name = parse_string();
+      auto name = parse_string();
+      if (!name) return error();
       skip_whitespace();
-      skip(':');
-      object[name] = Value{parse_value()};
-      skip(',');
+      if (!expect(':')) return error();
+      auto value = parse_value();
+      if (!value) return error();
+      object.emplace(*name, std::move(*value));
+      expect(',');
       skip_whitespace();
     }
-    skip('}');
+    if (!expect('}')) return error();
 
     return object;
   }
 
-  inline array_t parse_array() noexcept {
+  [[nodiscard]] inline expected_t<array_t> parse_array() noexcept {
     array_t array;
 
-    skip('[');
+    if (!expect('[')) return error();
     skip_whitespace();
-    while (peek() != ']') {
+    while (!view_.empty() && peek() != ']') {
       skip_whitespace();
-      array.emplace_back(parse_value());
-      skip(',');
+      auto value = parse_value();
+      if (!value) return error();
+      array.emplace_back(std::move(*value));
+      expect(',');
       skip_whitespace();
     }
-    skip(']');
+    if (!expect(']')) return error();
 
     return array;
   }
 
-  inline string_t parse_string() noexcept {
+  [[nodiscard]] inline expected_t<string_t> parse_string() noexcept {
     const auto parse = [this]() -> string_t {
       const auto is_string = [](const char ch) { return ch != '"'; };
       auto text = view_ | std::views::take_while(is_string);
       auto n = std::ranges::distance(text);
-      return take(n);
+      return unescape_string(take(n));
     };
 
-    skip('"');
+    if (!expect('"')) return error();
     string_t string = parse();
-    skip('"');
+    if (!expect('"')) return error();
     return string;
   }
 
-  inline int parse_number() noexcept {
+  [[nodiscard]] inline expected_t<int> parse_number() noexcept {
     auto text = view_ | std::views::take_while(is_digit);
     auto n = std::ranges::distance(text);
+    if (!n) return error();
     return to_int(take(n));
   }
 
-  inline bool parse_boolean() noexcept {
+  [[nodiscard]] inline expected_t<bool> parse_boolean() noexcept {
     if (view_.starts_with("true")) {
       view_.remove_prefix(4);
       return true;
@@ -171,14 +203,15 @@ private:
       view_.remove_prefix(5);
       return false;
     }
-    return false;
+    return error();
   }
 
-  inline nullptr_t parse_null() noexcept {
+  [[nodiscard]] inline expected_t<nullptr_t> parse_null() noexcept {
     if (view_.starts_with("null")) {
       view_.remove_prefix(4);
+      return nullptr;
     }
-    return nullptr;
+    return error();
   }
 
   [[nodiscard]] constexpr char peek() const noexcept {
@@ -191,8 +224,10 @@ private:
     return std::string{view};
   }
 
-  inline void skip(const char ch) noexcept {
-    if (view_.starts_with(ch)) view_.remove_prefix(1);
+  inline expected_t<char> expect(const char ch) noexcept {
+    if (!view_.starts_with(ch)) return error();
+    view_.remove_prefix(1);
+    return ch;
   };
 
   inline void skip_whitespace() noexcept {
@@ -204,24 +239,24 @@ private:
     }
   }
 
+  [[nodiscard]] static inline std::string unescape_string(std::string input) noexcept {
+    static std::regex reserved("\\\\([\"\\\\])");
+    auto output = std::regex_replace(input, reserved, "$1");
+    return output;
+  };
+
   std::string input_;
   std::string_view view_;
-  value_t value_;
 };
 
 [[nodiscard]] inline Value parse(std::string_view input) noexcept {
-  return Value{input};
+  Parser parser{input};
+  return parser.parse();
 }
 
 [[nodiscard]] inline std::string escape_string(std::string input) noexcept {
   static std::regex reserved(R"(["\\])");
   return std::regex_replace(input, reserved, R"(\$&)");
-};
-
-[[nodiscard]] inline std::string unescape_string(std::string input) noexcept {
-  static std::regex reserved("\\\\([\"\\\\])");
-  auto output = std::regex_replace(input, reserved, "$1");
-  return output;
 };
 
 [[nodiscard]] inline std::string serialize(
