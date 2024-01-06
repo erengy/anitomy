129904a8ed19c0361be85f6d1ba4cb7ee1599c11
Eren Okka <erenokka@gmail.com>
2024-01-07 00:38:44 +0300

    Simplify episode title parsing
    Removing the do-while loop does not affect any test cases.

--- include/anitomy/detail/parser/episode_title.hpp
+++ include/anitomy/detail/parser/episode_title.hpp
@@ -10,45 +10,34 @@
 
 namespace anitomy::detail {
 
-// @TODO: https://github.com/erengy/anitomy/issues/13
 inline std::optional<Element> parse_episode_title(std::span<Token> tokens) noexcept {
-  auto token_begin = tokens.begin();
-  auto token_end = tokens.begin();
+  // Find the first free unenclosed range
+  // e.g. `[Group] Title - Episode - Episode Title [Info]`
+  //                                 ^-------------^
+  auto token_begin = std::ranges::find_if(
+      tokens, [](const Token& token) { return is_free_token(token) && !token.is_enclosed; });
+  auto token_end = std::find_if(token_begin, tokens.end(), [](const Token& token) {
+    return is_open_bracket_token(token) || token.element_kind.has_value();
+  });
 
-  do {
-    // Find the first free unenclosed token
-    token_begin = std::ranges::find_if(token_end, tokens.end(), [](const Token& token) {
-      return is_free_token(token) && !token.is_enclosed;
-    });
-    if (token_begin == tokens.end()) break;
+  auto span = std::span{token_begin, token_end};
 
-    // Continue until a bracket or identifier is found
-    token_end = std::ranges::find_if(token_begin, tokens.end(), [](const Token& token) {
-      return token.kind == TokenKind::OpenBracket || token.element_kind.has_value();
-    });
+  // Trim delimiters
+  while (!span.empty() && is_delimiter_token(span.back())) {
+    span = span.first(span.size() - 1);
+  }
 
-    // @TODO: Ignore if it's only a dash
-
-    auto span = std::span(token_begin, token_end);
-
-    // Trim delimiters
-    while (!span.empty() && span.back().kind == TokenKind::Delimiter) {
-      span = span.first(span.size() - 1);
+  // Build episode title
+  if (std::string value = build_element_value(span); !value.empty()) {
+    for (auto& token : span) {
+      token.element_kind = ElementKind::EpisodeTitle;
     }
-
-    // Build episode title
-    if (std::string value = build_element_value(span); !value.empty()) {
-      for (auto& token : span) {
-        token.element_kind = ElementKind::EpisodeTitle;
-      }
-      return Element{
-          .kind = ElementKind::EpisodeTitle,
-          .value = value,
-          .position = span.front().position,
-      };
-    }
-    break;
-  } while (token_begin != tokens.end());
+    return Element{
+        .kind = ElementKind::EpisodeTitle,
+        .value = value,
+        .position = span.front().position,
+    };
+  }
 
   return std::nullopt;
 }
