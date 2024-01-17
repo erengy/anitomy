64a7d99e236cff77128b399d83b0af0cd58ac106
Eren Okka <erenokka@gmail.com>
2024-01-17 07:04:11 +0300

    Parse release group before file extension

--- include/anitomy/detail/parser/release_group.hpp
+++ include/anitomy/detail/parser/release_group.hpp
@@ -23,12 +23,31 @@ inline std::optional<Element> parse_release_group(std::span<Token> tokens) noexc
   });
 
   // Skip if the range contains other tokens
-  if (auto token = find_prev_token(tokens, token_begin, is_not_delimiter_token);
-      token != tokens.end() && !is_open_bracket_token(*token)) {
-    return parse_release_group({token_end, tokens.end()});
+  if (token_begin != tokens.end()) {
+    if (auto token = find_prev_token(tokens, token_begin, is_not_delimiter_token);
+        token != tokens.end() && !is_open_bracket_token(*token)) {
+      return parse_release_group({token_end, tokens.end()});
+    }
+    if (token_end != tokens.end() && !is_close_bracket_token(*token_end)) {
+      return parse_release_group({token_end, tokens.end()});
+    }
   }
-  if (token_end != tokens.end() && !is_close_bracket_token(*token_end)) {
-    return parse_release_group({token_end, tokens.end()});
+
+  // Check the last token before file extension
+  // e.g. `Title.Episode.Info-Group.mkv`
+  //                          ^----^
+  if (token_begin == tokens.end()) {
+    auto token = find_prev_token(tokens, tokens.end(), [](const Token& token) {
+      return token.element_kind != ElementKind::FileExtension && is_not_delimiter_token(token);
+    });
+    if (token != tokens.end() && is_free_token(*token)) {
+      auto prev_token = find_prev_token(tokens, token, [](const Token&) { return true; });
+      if (prev_token != tokens.end() && is_delimiter_token(*prev_token) &&
+          prev_token->value == "-") {
+        token_begin = token;
+        token_end = std::next(token);
+      }
+    }
   }
 
   auto span = std::span{token_begin, token_end};
