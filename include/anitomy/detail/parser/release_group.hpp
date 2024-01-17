6ff45c831eb48a8868834e28937cfdb81c84c194
Eren Okka <erenokka@gmail.com>
2024-01-17 07:20:07 +0300

    Split finder logic in release group parser

--- include/anitomy/detail/parser/release_group.hpp
+++ include/anitomy/detail/parser/release_group.hpp
@@ -11,32 +11,32 @@
 
 namespace anitomy::detail {
 
-inline std::optional<Element> parse_release_group(std::span<Token> tokens) noexcept {
+inline std::span<Token> find_release_group(std::span<Token> tokens) noexcept {
   // Find the first free enclosed range
   // e.g. `[Group] Title - Episode [Info]`
   //        ^----^
-  auto token_begin = std::ranges::find_if(tokens, [](const Token& token) {
+  auto first = std::ranges::find_if(tokens, [](const Token& token) {
     return is_free_token(token) && token.is_enclosed;  //
   });
-  auto token_end = std::find_if(token_begin, tokens.end(), [](const Token& token) {
+  auto last = std::find_if(first, tokens.end(), [](const Token& token) {
     return is_close_bracket_token(token) || is_identified_token(token);
   });
 
   // Skip if the range contains other tokens
-  if (token_begin != tokens.end()) {
-    if (auto token = find_prev_token(tokens, token_begin, is_not_delimiter_token);
+  if (first != tokens.end()) {
+    if (auto token = find_prev_token(tokens, first, is_not_delimiter_token);
         token != tokens.end() && !is_open_bracket_token(*token)) {
-      return parse_release_group({token_end, tokens.end()});
+      return find_release_group({last, tokens.end()});
     }
-    if (token_end != tokens.end() && !is_close_bracket_token(*token_end)) {
-      return parse_release_group({token_end, tokens.end()});
+    if (last != tokens.end() && !is_close_bracket_token(*last)) {
+      return find_release_group({last, tokens.end()});
     }
   }
 
-  // Check the last token before file extension
+  // Fall back to the last token before file extension
   // e.g. `Title.Episode.Info-Group.mkv`
   //                          ^----^
-  if (token_begin == tokens.end()) {
+  if (first == tokens.end()) {
     auto token = find_prev_token(tokens, tokens.end(), [](const Token& token) {
       return token.element_kind != ElementKind::FileExtension && is_not_delimiter_token(token);
     });
@@ -44,27 +44,31 @@ inline std::optional<Element> parse_release_group(std::span<Token> tokens) noexc
       auto prev_token = find_prev_token(tokens, token, [](const Token&) { return true; });
       if (prev_token != tokens.end() && is_delimiter_token(*prev_token) &&
           prev_token->value == "-") {
-        token_begin = token;
-        token_end = std::next(token);
+        first = token;
+        last = std::next(token);
       }
     }
   }
 
-  auto span = std::span{token_begin, token_end};
+  return {first, last};
+}
 
-  // Build release group
-  if (std::string value = build_element_value(span, KeepDelimiters::Yes); !value.empty()) {
-    for (auto& token : span) {
-      token.element_kind = ElementKind::ReleaseGroup;
-    }
-    return Element{
-        .kind = ElementKind::ReleaseGroup,
-        .value = value,
-        .position = span.front().position,
-    };
+inline std::optional<Element> parse_release_group(std::span<Token> tokens) noexcept {
+  const auto span = find_release_group(tokens);
+  if (span.empty()) return {};
+
+  std::string value = build_element_value(span, KeepDelimiters::Yes);
+  if (value.empty()) return {};
+
+  for (auto& token : span) {
+    token.element_kind = ElementKind::ReleaseGroup;
   }
 
-  return std::nullopt;
+  return Element{
+      .kind = ElementKind::ReleaseGroup,
+      .value = std::move(value),
+      .position = span.front().position,
+  };
 }
 
 }  // namespace anitomy::detail
