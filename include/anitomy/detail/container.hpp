680f45294dc12c25e93df17f17eb18b471da984d
Eren Okka <erenokka@gmail.com>
2025-03-24 08:45:26 +0300

    Fix previous token finder result

--- include/anitomy/detail/container.hpp
+++ include/anitomy/detail/container.hpp
@@ -14,7 +14,7 @@ template <typename Container, typename It = Container::iterator>
 inline It find_prev_token(Container& container, It it, token_predicate_t predicate) noexcept {
   if (it == container.begin()) return container.end();
   auto [token, _] = std::ranges::find_last_if(container.begin(), it, predicate);
-  return token;
+  return token == it ? container.end() : token;
 }
 
 template <typename Container, typename It = Container::iterator>
