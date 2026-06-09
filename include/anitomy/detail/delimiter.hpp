44efa8f0eb31be47d36ff23a4769a6baf6fb5657
GravyUnknown <abdullahbinnur89@gmail.com>
2026-06-09 20:22:36 +0600

    Add colon as delimiter
    Fixes #37.

--- include/anitomy/detail/delimiter.hpp
+++ include/anitomy/detail/delimiter.hpp
@@ -40,6 +40,7 @@ namespace anitomy::detail {
     case U'~':  // used for episode ranges
     case U'+':  // used in torrent titles
     case U'|':  // used in torrent titles, reserved in Windows
+    case U':':  // used in torrent titles, reserved in Windows
       return true;
     default:
       return is_space(ch) || is_dash(ch);
